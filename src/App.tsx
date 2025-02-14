3dd9e5bc3f6b8bb4fcc2a6c381be5243b8ab8e61
Baptiste LACROIX <baptiste.lacroix03@gmail.com>
2025-02-14 14:19:46 +0100

    chores: init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { ThemeSwitch } from "@/components/theme-switch.tsx";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World !</h1>
+      <ThemeSwitch />
+    </>
+  );
+}
+
+export default App;
