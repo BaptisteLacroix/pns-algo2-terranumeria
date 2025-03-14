2e9706de2d55be91eecc85ed8de17dded1bee00d
Baptiste LACROIX <baptiste.lacroix03@gmail.com>
2025-03-14 15:19:04 +0100

    feat: adding apprentissage documentation

--- frontend/src/components/SidePanelLeft.tsx
+++ frontend/src/components/SidePanelLeft.tsx
@@ -1,82 +1,87 @@
 import {
-  Card,
-  CardHeader,
-  CardBody,
-  Image,
-  Spacer,
-  Link,
-  Button,
+    Card,
+    CardHeader,
+    CardBody,
+    Image,
+    Spacer,
+    Link,
+    Button,
 } from "@heroui/react";
 
 export const SidePanelLeft = () => {
-  return (
-    <div className="flex flex-col h-screen w-1/6 shadow-xl">
-      <Spacer y={4} />
-      <Image alt="heroui logo" src="/logo.png" className="rounded-none" />
-      <div className="flex flex-col justify-between h-full pr-5 pl-5 pb-20">
-        <Card className="rounded-none shadow-none">
-          <CardHeader className="flex gap-3 rounded-none">
-            <h1 className="text-3xl">Documentation</h1>
-          </CardHeader>
-          <CardBody className="overflow-hidden pl-0 pr-0" >
-            <ul className="flex flex-col">
-              <li className="hover:bg-yellow  w-full">
-                <Link
-                  href="#"
-                  isBlock
-                  color="undefined"
-                  className="text-color-darkblue font-semibold text-xl h-12 ml-5"
+    return (
+        <div className="flex flex-col h-screen w-1/6 shadow-xl">
+            <Spacer y={4}/>
+            <Image alt="heroui logo" className="rounded-none" src="/logo.png"/>
+            <div className="flex flex-col justify-between h-full pr-5 pl-5 pb-20">
+                <Card className="rounded-none shadow-none">
+                    <CardHeader className="flex gap-3 rounded-none">
+                        <h1 className="text-3xl">Documentation</h1>
+                    </CardHeader>
+                    <CardBody className="overflow-hidden pl-0 pr-0">
+                        <ul className="flex flex-col">
+                            <li className="hover:bg-yellow  w-full">
+                                <Link
+                                    isBlock
+                                    className="text-color-darkblue font-semibold text-xl h-12 ml-5"
+                                    color="undefined"
+                                    href={"/learning"}
+                                >
+                                    L'entraînement
+                                </Link>
+                            </li>
+                            <li className="hover:bg-yellow  w-full">
+                                <Link
+                                    isBlock
+                                    className="text-color-darkblue font-semibold text-xl h-12 ml-5"
+                                    color="undefined"
+                                    href="#"
+                                >
+                                    Les biais
+                                </Link>
+                            </li>
+                            <li className="hover:bg-yellow  w-full">
+                                <Link
+                                    isBlock
+                                    className="text-color-darkblue font-semibold text-xl w-full h-12 ml-5"
+                                    color="undefined"
+                                    href="#"
+                                >
+                                    La véracité
+                                </Link>
+                            </li>
+                            <li className="hover:bg-yellow  w-full">
+                                <Link
+                                    isBlock
+                                    className="text-color-darkblue font-semibold text-xl w-full h-12 ml-5"
+                                    color="undefined"
+                                    href="#"
+                                >
+                                    Les mathématiques
+                                </Link>
+                            </li>
+                            <li className="hover:bg-yellow  w-full">
+                                <Link
+                                    isBlock
+                                    className="text-color-darkblue font-semibold text-xl w-full h-12 ml-5"
+                                    color="undefined"
+                                    href="#"
+                                >
+                                    L'espace vectoriel
+                                </Link>
+                            </li>
+                        </ul>
+                    </CardBody>
+                </Card>
+                <Button
+                    className="pr-5 pl-5 hover:bg-yellow  text-white font-semibold  text-lg"
+                    color="primary"
+                    radius="none"
+                    size="lg"
                 >
-                  L'entraînement
-                </Link>
-              </li>
-              <li className="hover:bg-yellow  w-full">
-                <Link
-                  href="#"
-                  isBlock
-                  color="undefined"
-                  className="text-color-darkblue font-semibold text-xl h-12 ml-5"
-                >
-                  Les biais
-                </Link>
-              </li>
-              <li className="hover:bg-yellow  w-full">
-                <Link
-                  href="#"
-                  isBlock
-                  color="undefined"
-                  className="text-color-darkblue font-semibold text-xl w-full h-12 ml-5"
-                >
-                  La véracité
-                </Link>
-              </li>
-              <li className="hover:bg-yellow  w-full">
-                <Link
-                  href="#"
-                  isBlock
-                  color="undefined"
-                  className="text-color-darkblue font-semibold text-xl w-full h-12 ml-5"
-                >
-                  Les mathématiques
-                </Link>
-              </li>
-              <li className="hover:bg-yellow  w-full">
-                <Link
-                  href="#"
-                  isBlock
-                  color="undefined"
-                  className="text-color-darkblue font-semibold text-xl w-full h-12 ml-5"
-                >
-                  L'espace vectoriel
-                </Link>
-              </li>
-            </ul>
-          </CardBody>
-        </Card>
-        <Button size="lg" radius="none" color ="primary" className="pr-5 pl-5 hover:bg-yellow  text-white font-semibold  text-lg">
-          Nouvelle conversation
-        </Button>
-      </div>
-    </div>
-  );
+                    Nouvelle conversation
+                </Button>
+            </div>
+        </div>
+    );
 };
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,26 @@
-import { SidePanelLeft } from "@/components/SidePanelLeft.tsx";
-import { SidePanelRight } from "@/components/SidePanelRight.tsx";
-import { DialogBox } from "@/components/DialogBox.tsx";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {SidePanelLeft} from "@/components/SidePanelLeft.tsx";
+import {SidePanelRight} from "@/components/SidePanelRight.tsx";
+import {DialogBox} from "@/components/DialogBox.tsx";
+import {Learning} from "@/pages/learning.tsx";
 
 export const TerraNumerIA = () => {
-  return (
-    <div className="flex w-full h-full">
-      <SidePanelLeft />
-      <DialogBox />
-      <SidePanelRight />
-    </div>
-  );
+    return (
+        <Router>
+            <div className="flex w-full h-full">
+                <SidePanelLeft/>
+                <div className="flex-1">
+                    <Routes>
+                        <Route path="/" element={<DialogBox/>}/>
+                        <Route path="/learning" element={<Learning/>}/>
+                        <Route path="/biais" element={<Learning/>}/>
+                        <Route path="/veracite" element={<Learning/>}/>
+                        <Route path="/mathematiques" element={<Learning/>}/>
+                        <Route path="/espace-vectoriel" element={<Learning/>}/>
+                    </Routes>
+                </div>
+                <SidePanelRight/>
+            </div>
+        </Router>
+    );
 };
